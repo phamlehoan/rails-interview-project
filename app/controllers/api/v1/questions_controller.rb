3e85d056aed245d3d94b4c81de89ac20a30f61c7
CW-HoanPL <CW-HoanPL@github.com>
2022-03-03 23:56:22 +0700

    Require API Key

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V1
     class QuestionsController < ApplicationController
+      before_action :api_key_valid?
+
       def index
         questions = Question.where(private: false).includes(:user, :answers)
         render json: questions, each_serializer: QuestionSerializer
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+  def api_key_valid?
+    if request.headers["X-Api-Key"]
+      api_key = request.headers["X-Api-Key"]
+      tenant = Tenant.find_by(api_key: api_key)
+      if tenant
+        @tenant = tenant
+      else
+        render json: {status: 'ERROR', message: 'Invalid API key'}, status: :unauthorized
+      end
+    else
+      render json: {status: 'ERROR', message: 'No API key'}, status: :unauthorized
+    end
+  end
 end
